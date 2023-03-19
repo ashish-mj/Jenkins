f4eb1c4670285c5074ec3c345632c66efea70b9d
Ashish M J <ashm.jagadeesh@gmail.com>
2023-03-19 17:52:02 +0530

    Modify function name

--- testRoutes.py
+++ testRoutes.py
@@ -1,6 +1,6 @@
 from app import *
 
-def testHome():
+def test_Home():
     response = app.test_client().get('/')
     assert b"Welcome to Jenkins Tutorials" in response.data
     assert response.status_code == 200
